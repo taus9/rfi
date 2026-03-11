ddf53adaceafabb27691b786e2480da02d38a3d1
Christopher Martinez <crmarz@proton.me>
2026-03-11 17:01:49 -0400

    Refactor args output formatting

---
+++ src/file/mod.rs
@@ -0,0 +1,9 @@
+pub struct File;
+
+impl File {
+
+    pub fn run(file: &String) -> Result<(), String> {
+        Ok(())
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use crate::{args::Args, repl::Repl};
+use crate::{args::Args, repl::Repl, file::File};
 
 mod builtin;
 mod vm;
 mod args;
 mod repl;
+mod file;
 
-const RUN_FILE: &str = "-f";
+const RFI_VERSION: &str = "rusty forth interpreter 0.1.0";
+
+const DASH_F: &str = "-f";
+const DASH_H: &str = "-h";
+const DASH_V: &str = "-v";
+
+const COMPLETE: &str = "-> program complete <-";
+const USAGE: &str = "Usage: rfi -f [filename]";
+const UNKNOWN: &str = "Unknown command:";
+const HELP: &str = "Use -h to get help";
+
+const HELP_TEXT: &str = "\
+Usage: rfi [options]
+
+  -f <file>  Run a file
+  -h         Show this help
+  -v         Show version
+
+  If no arguments are provided, the interactive REPL will start.";
 
 fn main() {
 
@@ -21,11 +40,29 @@ fn main() {
 
         while let Some(arg) =  args.next_arg() {
             match arg.as_str() {
-                RUN_FILE => {
-                    println!("run file!");
+                DASH_F => {
+                    if let Some(file) = args.next_arg() {
+                        
+                        match File::run(&file) {
+                            Ok(_) => println!("{}", COMPLETE),
+                            Err(e) => eprintln!("{}", e),
+                        }
+
+                    } else {
+                        eprintln!("{}", USAGE);
+                    }
                 }
+
+                DASH_H => {
+                    println!("{}", HELP_TEXT);
+                }
+
+                DASH_V => {
+                    println!("{}", RFI_VERSION);
+                }
+
                 _ => {
-                    println!("unknown command '{}'", arg);
+                    println!("{} {}\n{}", UNKNOWN, arg, HELP);
                 }
             }
         }
